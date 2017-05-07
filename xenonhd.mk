9844ec234ab930b4c6d9b8df6572350cd829a8ea
ljjehl <ljjehl@gmail.com>
2017-05-07 19:21:56 +0200

    Update maintainer info
    Change-Id: Ic2322fde06c73b13807ff99e802c1b5833aafa10

--- xenonhd.mk
+++ xenonhd.mk
@@ -3,6 +3,15 @@ $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
+ROOT_METHOD=magisk
+
+PRODUCT_PACKAGES += \
+    Adaway \
+    KernelAdiutor \
+    MiXplorer
+
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="ljjehl"
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_BRAND := htc
